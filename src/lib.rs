b2ce704fdd5c759909dc67903ffc7e185b542bd7
Kağan Kayal <kagan@kagan.de>
2017-11-15 21:43:17 +0100

    Improved the readme and rustdoc

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,8 @@
 //!
 //! # Examples
 //!
+//! ## Example 1: 
+//! 
 //! To extract 3 bits starting at bit index 5 within a byte (0xFF) and interpret them as an unsigned integer
 //!
 //! ```rust
@@ -23,6 +25,17 @@
 //! assert_eq!(b, 7);
 //! ```
 //! 
+//! ## Example 2: 
+//! 
+//! The data source is a vector of u8 types. We want to go to byte offset 1, 
+//! bit offset 7 and starting from there extract 3 bits as an u16
+//! 
+//! ```rust
+//! use bitlab::*;
+//! let v: Vec<u8> = vec!{ 0x48, 0x61, 0x6C, 0x6C, 0x6F }; // = "Hallo"
+//! let bar = u16(&v, 1, 7, 3); // relevant bytes = 0x6161 = 0b0110000  --> 101 <-- 101100
+//! assert_eq!(bar.unwrap(), 5);
+//! ```
 //!
 //! # MIT Licence
 //!
